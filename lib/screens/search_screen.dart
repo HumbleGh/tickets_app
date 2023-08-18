d8413171aa297e2d25bd663bc50f62d200cf53bf
HumbleGh <isaacoseianane@gmail.com>
2023-08-18 11:19:53 +0000

    Made Flight dynamic

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:tickets_app/utils/app_layout.dart';
+import 'package:tickets_app/widgets/icon_text_widget.dart';
 import '../utils/app_style.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -17,12 +18,10 @@ class SearchScreen extends StatelessWidget {
               vertical: AppLayout.getHeight(20)),
           children: [
             Gap(AppLayout.getHeight(40)),
-            Container(
-              child: Text(
-                'What are\n you looking for?',
-                style: Styles.headLineStyle1
-                    .copyWith(fontSize: AppLayout.getHeight(35)),
-              ),
+            Text(
+              'What are\n you looking for?',
+              style: Styles.headLineStyle1
+                  .copyWith(fontSize: AppLayout.getHeight(35)),
             ),
             Gap(AppLayout.getHeight(10)),
             FittedBox(
@@ -60,25 +59,10 @@ class SearchScreen extends StatelessWidget {
                   )),
             ),
             Gap(AppLayout.getHeight(25)),
-            Container(
-              padding: EdgeInsets.symmetric(
-                  vertical: AppLayout.getWidth(12),
-                  horizontal: AppLayout.getHeight(20)),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
-              ),
-              child: Row(
-                children: [
-                  const Icon(Icons.flight_takeoff_rounded),
-                  Gap(AppLayout.getHeight(15)),
-                  Text(
-                    'Departure',
-                    style: Styles.textStyle,
-                  )
-                ],
-              ),
-            )
+            const AppIconText(
+                icon: Icons.flight_takeoff_rounded, text: 'Departure'),
+            Gap(AppLayout.getHeight(25)),
+            const AppIconText(icon: Icons.flight_land_rounded, text: 'Arival'),
           ],
         ));
   }
--- lib/widgets/icon_text_widget.dart
+++ lib/widgets/icon_text_widget.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_layout.dart';
+import '../utils/app_style.dart';
 
 class AppIconText extends StatelessWidget {
-  const AppIconText({super.key});
+  final IconData icon;
+  final String text;
+  const AppIconText({super.key, required this.icon, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: AppLayout.getWidth(12),
+          horizontal: AppLayout.getHeight(20)),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.grey,
+          ),
+          Gap(AppLayout.getHeight(15)),
+          Text(
+            text,
+            style: Styles.textStyle,
+          )
+        ],
+      ),
+    );
   }
 }
