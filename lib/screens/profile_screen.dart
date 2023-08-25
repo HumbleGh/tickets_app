b5e959f9ed97f7121b26b992f44b8216e2668aca
HumbleGh <isaacoseianane@gmail.com>
2023-08-25 14:46:03 +0000

    minor updates

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -13,6 +13,8 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
+    // final size = AppLayout.getSize(context);
+
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: SafeArea(
@@ -20,9 +22,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
           children: [
             Container(
               padding: EdgeInsets.symmetric(
-                  horizontal: AppLayout.getHeight(16),
-                  vertical: AppLayout.getHeight(16)),
+                  horizontal: AppLayout.getHeight(20),
+                  vertical: AppLayout.getHeight(30)),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
                     height: AppLayout.getHeight(80),
@@ -35,7 +38,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             image: AssetImage('assets/images/gh.jpg'),
                             fit: BoxFit.cover)),
                   ),
-                  Gap(AppLayout.getHeight(8)),
+                  Gap(AppLayout.getHeight(12)),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -53,18 +56,66 @@ class _ProfileScreenState extends State<ProfileScreen> {
                             vertical: AppLayout.getHeight(2)),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(4),
-                            color: Colors.white),
-                        child: const Row(
+                            color: Colors.white70),
+                        child: Row(
                           children: [
-                            Icon(
+                            const Icon(
                               Icons.alt_route,
                               size: 17,
                             ),
-                            Text('Premium Status')
+                            Text(
+                              'Premium Status',
+                              style: Styles.headLineStyle3,
+                            )
                           ],
                         ),
                       )
                     ],
+                  ),
+                  Gap(AppLayout.getHeight(80)),
+                  Column(
+                    children: [
+                      Text(
+                        'Edit',
+                        style: Styles.textStyle,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: AppLayout.getWidth(20),
+                  vertical: AppLayout.getHeight(5)),
+              child: Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: AppLayout.getWidth(15),
+                        vertical: AppLayout.getHeight(15)),
+                    height: AppLayout.getHeight(100),
+                    // width: size.width * 0.40,
+                    decoration: BoxDecoration(
+                        color: Colors.lightBlueAccent,
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(12))),
+                    child: Text(
+                        'Hey! Chale whatever you want to say biaa, edey your there man. Yeah!, I mean it ',
+                        style: Styles.headLineStyle3
+                            .copyWith(color: Colors.white)),
+                  ),
+                  Positioned(
+                    right: -45,
+                    top: -40,
+                    child: Container(
+                      padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(width: 18, color: Colors.lightBlue),
+                          shape: BoxShape.circle,
+                          color: Colors.transparent),
+                    ),
                   )
                 ],
               ),
