1f08571ed31636a768d815c1a8253926c07237dd
HumbleGh <isaacoseianane@gmail.com>
2023-08-28 19:22:54 +0000

    minor updates

---
+++ lib/widgets/tab_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabBarWidget extends StatefulWidget {
+  const TabBarWidget({super.key});
+
+  @override
+  State<TabBarWidget> createState() => _TabBarWidgetState();
+}
+
+class _TabBarWidgetState extends State<TabBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
